0e949d94f82b7510c72d8d939b5151a0eb61fad3
KBandipo <keneebandipo@gmail.com>
2023-10-08 22:56:02 +0100

    Create function createTask

--- Resources/JavaScript/script.js
+++ Resources/JavaScript/script.js
@@ -2,3 +2,14 @@
 const textInput = document.querySelector(".text-input");
 const addButton = document.querySelector(".add");
 const listContainer = document.querySelector("#list-container");
+
+// Create Function of new task item
+function createTask(toDoText) {
+  const newTextContent = document.createElement("li");
+  newTextContent.innerHTML = `
+  <div class = "to-do-inner-container"><input type="checkbox" class="checkbox"/><span>${toDoText}</span></div>
+        <button class="delete-content">X</button>
+    `;
+
+  return newTextContent;
+}
