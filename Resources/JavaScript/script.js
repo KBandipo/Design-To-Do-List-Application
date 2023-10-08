818bc1b8c5bba321edc8e2e351f4844587b13fcd
KBandipo <keneebandipo@gmail.com>
2023-10-08 22:53:46 +0100

    Get elements to the DOM

---
+++ Resources/JavaScript/script.js
@@ -0,0 +1,4 @@
+// Get elements
+const textInput = document.querySelector(".text-input");
+const addButton = document.querySelector(".add");
+const listContainer = document.querySelector("#list-container");
